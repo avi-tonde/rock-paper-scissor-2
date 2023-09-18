51369506f5a0b52be0e760656721c9c025327c77
avi-tonde <a9284556060@gmail.com>
2023-09-18 13:43:05 +0530

    Game functionality implimented with UI

--- script.js
+++ script.js
@@ -6,9 +6,16 @@ userChoice.forEach(getUserSelection => {
         const computerSelection = computerChoice(); //get computer selection
 
         checkWinner(userSelection, computerSelection); // checking winner
-       
+
+        document.getElementById("userChoice").src = "Resources/icons/" + userSelection + ".svg";
+        document.getElementById("pcChoice").src = "Resources/icons/" + computerSelection + ".svg";
     })
 })
+
+const playArea = document.getElementById('playArea');
+const showResult = document.getElementById('showResult');
+const result = document.getElementById('result');
+
 
 
 //Create Computer Choice
@@ -30,41 +37,80 @@ function computerChoice() {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 //Function to check the winner
-function checkWinner(userSelection, computerSelection ){
-    if(computerSelection == userSelection){
+function checkWinner(userSelection, computerSelection) {
+
+    if (computerSelection == userSelection) {
         console.log("TIE");
-    }else if (computerSelection == 'rock' && userSelection =='scissor') {
-        console.log("Computer Own")
-    }else if (computerSelection == 'scissor' && userSelection =='paper') {
-        console.log("Computer Own")
-    }else if (computerSelection == 'paper' && userSelection =='rock') {
-        console.log("Computer Own")
-    }else{
+        tie();
+    } else if (computerSelection == 'rock' && userSelection == 'scissor') {
+        console.log("Computer Own");
+        computerWin();
+    } else if (computerSelection == 'scissor' && userSelection == 'paper') {
+        console.log("Computer Own");
+        computerWin();
+    } else if (computerSelection == 'paper' && userSelection == 'rock') {
+        console.log("Computer Own");
+        computerWin();
+    } else {
         console.log('You Own');
+        youWin();
     }
 }
 
+function computerWin() {
+    playArea.style.display = "none";
+    showResult.style.display = "flex";
+    btnEffect('.pc-win', 'add');
+}
+function youWin() {
+    playArea.style.display = "none";
+    showResult.style.display = "flex";
+    btnEffect('.you-win', 'add');
+}
+function tie() {
+    playArea.style.display = "none";
+    showResult.style.display = "flex";
+    result.innerText = "TIE";
+    // btnEffect('.you-win', 'add');
+}
+
+function btnEffect(classname, action) {
+    if (action == 'add') {
+        const elements = document.querySelectorAll(classname);
+        elements[0].classList.add('bgc3');
+        elements[1].classList.add('bgc2');
+        elements[2].classList.add('bgc1');
+        if (classname == '.you-win') {
+            result.innerText = "YOU WIN";
+        } else if (classname == '.pc-win') {
+            result.innerText = "YOU LOST";
+        }
+    }
+    else {
+        const elements = document.querySelectorAll('.effect');
+        elements.forEach((element) => {
+            element.classList.remove("bgc3", "bgc2", "bgc1");
+        });
+
+        result.innerText = " ";
+    }
+}
+
+
+
+function playAgain() {
+    playArea.style.display = "flex";
+    showResult.style.display = "none";
+
+    btnEffect('', '')
+
+}
+
+function openModel() {
+
+}
+
+function closeModel() {
+
+}
